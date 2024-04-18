e72bb3962fdffbb85ccbc5a2fc2ccc18095031a4
Heyse <luizedumariz@gmail.com>
2024-04-18 18:02:15 -0300

    ihje

---
+++ Foguinho/Assets/Scripts/scriptfacil.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptfacil : MonoBehaviour
+{
+    public Gradient ok;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
